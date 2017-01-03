fa70da7aa7be3cdb64e395de082dd3a2291808c7
wpshadow <wpshadow74@gmx.at>
2017-01-04 00:09:45 +0100

    Mouseover Layer zu Gameobjects hinzugefügt

---
+++ tdDev/raw/showWindow.mm
@@ -0,0 +1,11 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1483470238002" ID="ID_1354105286" MODIFIED="1483470272050" TEXT="showWindow">
+<node CREATED="1483470405371" ID="ID_1487179296" MODIFIED="1483470629072" POSITION="right" TEXT="window_001: Main Game Window">
+<node CREATED="1483470631347" ID="ID_1979544404" MODIFIED="1483470678544" TEXT="window_000: Language"/>
+<node CREATED="1483470679698" ID="ID_1537219108" MODIFIED="1483470763902" TEXT="window_002: History"/>
+<node CREATED="1483470765090" ID="ID_210928800" MODIFIED="1483470889453" TEXT="window_003: Optionen"/>
+<node CREATED="1483470924320" ID="ID_741105990" MODIFIED="1483470936076" TEXT="window_004: Level Editor"/>
+</node>
+</node>
+</map>
